d54a5afdfe01b3a3d90fb0df70a68c4d8e4a2db8
unknown <alfarkaleel@gmail.com>
2020-05-14 03:38:58 +0530

    singleton

---
+++ SingletonPattern/src/Single.java
@@ -0,0 +1,71 @@
+
+public class Single {
+	
+	private static volatile int number = 0;
+	private static volatile Single obj;
+	
+	private Single() {
+		
+		if(obj != null ) {
+			
+			throw new RuntimeException("Please use method in this class in order to create instance");
+		}
+		
+		
+		
+		
+	}
+	
+	
+	public static Single getobj() {
+		
+		if (obj == null) {
+			
+			synchronized(Single.class) {
+				
+				if(obj == null) {
+					
+					obj = new Single();
+					
+				}
+				
+			}
+			
+			
+		}
+		
+		return obj;
+	}
+
+	
+	public static int getint() {
+		
+		if(number == 0) {
+			
+			synchronized(Single.class) {
+				
+				if(number == 0) {
+					
+					for(int i = 0;i<100000;i++) {
+						
+						number = i;
+						//doing something to elapse time
+						
+					}
+					
+					
+				}
+				
+			}
+			
+		}
+		
+		
+		return number;
+		
+	}
+	
+	
+	
+	
+}
---
+++ SingletonPattern/src/Testingsingle.java
@@ -0,0 +1,41 @@
+
+public class Testingsingle {
+	
+	public static void main(String []args) {
+		
+	
+	long start;
+	long end;
+	
+	start = System.currentTimeMillis();
+	
+	Single obj	= Single.getobj();
+	int num = Single.getint();
+	
+	end = System.currentTimeMillis();
+	
+	System.out.println("Returned number : "+ num);
+	System.out.println("Time taken for first object : " + (end-start));
+
+	
+	
+	
+   start = System.currentTimeMillis();
+	
+	Single obj1	= Single.getobj();
+	int num1 = Single.getint();
+	
+     end = System.currentTimeMillis();
+	
+ 	System.out.println("Returned number : "+ num1);
+	System.out.println("Time taken for second object : " + (end-start));
+	
+	
+	//System.out.println(single);
+	
+	//System.out.println(single1);
+		
+	}
+	
+
+}
